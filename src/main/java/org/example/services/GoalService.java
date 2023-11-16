1e4f1e7a801d6dc8e7b1b27af1955d244c09ab55
nadir inab <nadir.inab.dev@gmail.com>
2023-11-16 22:29:07 +0100

    data is well added

--- src/main/java/org/example/services/GoalService.java
+++ src/main/java/org/example/services/GoalService.java
@@ -1,4 +1,32 @@
 package org.example.services;
 
+import org.example.entities.Goal;
+import org.example.repo.GoalRepo;
+
+import java.util.List;
+import java.util.Optional;
+
 public class GoalService {
+
+    private final GoalRepo GoalRepo;
+
+    public GoalService (GoalRepo GoalRepo){
+        this.GoalRepo = GoalRepo;
+    }
+
+    public void saveGoal(Goal goal){
+        GoalRepo.save(goal);
+    }
+
+    public void deleteGoal(Goal goal){
+        GoalRepo.delete(goal);
+    }
+
+    public List<Goal> getAllGoals(){
+        return GoalRepo.findAll();
+    }
+
+    public Optional<Goal> getGoal(Long id){
+        return GoalRepo.findById(id);
+    }
 }
