ebc298d537ded00978fde214f7c924d200e45792
nadir inab <nadir.inab.dev@gmail.com>
2023-11-16 20:18:30 +0100

    Beans are well called

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -1,8 +1,10 @@
 package org.example;
 
 
+import org.example.entities.Goal;
 import org.example.entities.Task;
 import org.example.entities.User;
+import org.example.services.GoalService;
 import org.example.services.TaskServiceImplement;
 import org.example.services.UserServiceImplement;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
@@ -18,6 +20,9 @@ public class App
 
         TaskServiceImplement taskService = context.getBean("TaskService",TaskServiceImplement.class);
         Task task=context.getBean("Task", Task.class);
+
+        GoalService goalService = context.getBean("GoalService", GoalService.class) ;
+        Goal goal = context.getBean("Goal", Goal.class) ;
 
         System.out.println( "Hello World!" );
     }
